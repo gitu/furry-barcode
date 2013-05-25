c388eb8c92f0db94da075723b8e56555df752cab
gitu <github@schrag.org>
2013-05-25 11:53:39 +0200

    codes fixable

--- barcode.py
+++ barcode.py
@@ -13,13 +13,6 @@ from datetime import datetime
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-@app.route('/<code>')
-def coded(code):
-    value = request.args.get('value', '')
-    count = request.args.get('count', '')
-    scale = request.args.get('scale', '5')
-    return render_template('main.html', code=code, value=value, count=count, scale=scale)
-
 
 def genBarcode(code, value, canvas, scale):
     dr = createBarcodeDrawing(code, value=value, humanReadable=True)
@@ -31,45 +24,64 @@ def genBarcode(code, value, canvas, scale):
     canvas.drawString(1*mm, 1*mm, "generated at "+str(datetime.now()) + " from "+request.url)
     canvas.showPage()
 
-@app.route('/', methods=[ 'GET'])
-def form():
-    code = request.args.get('code', '-')
-    value = request.args.get('value', '-')
-    count = request.args.get('count', '-')
+
+def render(scale, showCodes, code=None, count="", value="", showImage=False, template='main.html'):
+    print "render called: " + str(showCodes)
+    codes = None
+    bcimage = None
+    if showCodes or code is None or not (code in getCodeNames()):
+        codes = getCodeNames()
+        action = url_for('switchcode')
+    else:
+        action = url_for('fixcode', code=code)
+    if showImage:
+        bcimage = url_for('img', code=code, scale=scale, value=value)
+    return render_template(template, codes=codes, code=code, count=count, value=value,
+                           scale=scale, bcimage=bcimage, action=action)
+
+
+def gen_form(code, showCodes):
+    value = request.args.get('value', '')
+    count = request.args.get('count', '')
     scale = request.args.get('scale', '5')
     action = request.args.get('action', '')
-    if len(value) == 0:
-        value = '0'
     if len(scale) == 0 or not scale.isdigit():
         scale = 5
-
-    if code == '-':
-        return render_template('main.html', codes=getCodeNames())
+    if len(value) == 0:
+        return render(scale, showCodes, code)
 
     try:
         if not (code in getCodeNames()):
             flash("Code "+code+" not supported allowed are : " + str(getCodeNames()))
-            return render_template('main.html', codes=getCodeNames(), count=count, value=value, scale=scale)
+            return render(scale, True, None, count, value)
 
         if value.isdigit() and count.isdigit():
-            if int(count) > 1000:
+            if int(count) > 1000 and action == 'Generate PDF':
                 flash("Please enter a lower count - maximum is 1000.")
-                return render_template('main.html', codes=getCodeNames(), code=code, count=count, value=value,
-                                       scale=scale)
+                return render(scale, showCodes, code, count, value)
 
+        dr = createBarcodeDrawing(code, value=value, humanReadable=True)
         if action == 'Generate PDF':
             if value.isdigit() and count.isdigit():
-                return redirect(url_for('pdflist', code=code, scale=int(scale), start=value, count=count))
+                return redirect(url_for('pdflist', code=code, scale=scale, start=value, count=count))
             else:
-                return redirect(url_for('pdf', code=code, scale=int(scale), value=value))
+                return redirect(url_for('pdf', code=code, scale=scale, value=value))
         else:
-            return render_template('main.html', codes=getCodeNames(), code=code, count=count, value=value, scale=scale,
-                                   bcimage=url_for('img', code=code, scale=int(scale), value=value))
-
-
+            return render(scale, showCodes, code, count, value, True)
     except:
         flash("Error while generating a " + code + ": " + str(sys.exc_info()[0]))
-        return render_template('main.html', code=code, codes=getCodeNames(), count=count, value=value, scale=scale)
+        return render(scale, showCodes, code, count, value)
+
+@app.route('/<code>')
+def fixcode(code):
+    return gen_form(code, False)
+
+
+@app.route('/')
+def switchcode():
+    code = request.args.get('code', '')
+    return gen_form(code, True)
+
 
 
 @app.route('/<code>/<int:scale>/<int:start>/<int:count>')
@@ -83,15 +95,35 @@ def pdflist(code, start, count, scale):
             genBarcode(code, str(start+i), p, scale)
     except:
         flash("Error while generating a " + code + ": " + str(sys.exc_info()[0]))
-        return render_template('main.html', code=code, codes=getCodeNames(), count=count, value=start, scale=scale)
+        return render(scale, True, code, count, start)
 
     p.save()
-
     pdf_out = output.getvalue()
     output.close()
 
     response = make_response(pdf_out)
     response.headers['Content-Disposition'] = "attachment; filename='"+code+"_"+str(start)+"_"+str(count)+".pdf"
+    response.mimetype = 'application/pdf'
+    return response
+
+
+@app.route('/<code>/<int:scale>/<path:value>')
+def pdf(code, value, scale):
+    output = cStringIO.StringIO()
+
+    p = canvas.Canvas(output,pagesize=landscape(A4))
+    try:
+        genBarcode(code, str(value), p, scale)
+    except:
+        flash("Error while generating a " + code + ": " + str(sys.exc_info()[0]))
+        return render(scale, True, code, None, value)
+
+    p.save()
+    pdf_out = output.getvalue()
+    output.close()
+
+    response = make_response(pdf_out)
+    response.headers['Content-Disposition'] = "attachment; filename='"+code+"_"+str(value)+".pdf"
     response.mimetype = 'application/pdf'
     return response
 
@@ -112,26 +144,6 @@ def img(code, value, scale):
 
 
 
-@app.route('/<code>/<int:scale>/<path:value>')
-def pdf(code, value, scale):
-    output = cStringIO.StringIO()
-
-    p = canvas.Canvas(output,pagesize=landscape(A4))
-    try:
-        genBarcode(code, str(value), p, scale)
-    except:
-        flash("Error while generating a " + code + ": " + str(sys.exc_info()[0]))
-        return render_template('main.html', code=code, codes=getCodeNames(), value=value, scale=scale)
-
-    p.save()
-
-    pdf_out = output.getvalue()
-    output.close()
-
-    response = make_response(pdf_out)
-    response.headers['Content-Disposition'] = "attachment; filename='"+code+"_"+str(value)+".pdf"
-    response.mimetype = 'application/pdf'
-    return response
 
 @app.route('/favicon.ico')
 def favicon():
